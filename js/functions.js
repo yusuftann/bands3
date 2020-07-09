e2bf955e964b1e2f665ebd650fe693af077f7b99
yusuftann <67969350+yusuftann@users.noreply.github.com>
2020-07-09 14:20:16 +0100

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+//Y.Tan
+//9th July 2020
+//Function practice
+
+// let x = 5;
+// let y = 10;
+
+// const magicNumber = () => {
+//     y++; 
+//     if (y % 2 == 0) 
+//     {x = x + 3;}
+// }
+
+// //x is 11 hunty
+
+// magicNumber();
+// magicNumber();
+// magicNumber();
+// magicNumber();
+// magicNumber();
+// //What is the value of X once this program has run
+// console.log(x)
+
+//function that takes two parameters for coffee 
+//order (size, type of drink)
+
+const takeOrder = (size, drinkType) => {
+    console.log("Order recieved" ${size} ${drinkType});
+}
+
+takeOrder("Tall", "Latte");
+
